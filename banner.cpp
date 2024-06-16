3443b9d0a73858d0bfe0b81593aaf395179416b8
frand-kod <frandkampus99@gmail.com>
2024-06-16 08:59:18 +0700

    inisialisasi judul nama dan beberapa atribut gakpenting

---
+++ banner.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void banner(){
+    cout<<"|>> ---------- SISTEM PARKIR KENDARAAN ---------- <<|"<<endl;
+    cout<<"|===================================================|"<<endl;
+
+    cout<<"|\nDAFTAR PERINTAH TERSEDIA : \n"<<endl;
+    cout <<"|-- Menu CRUD --|"<<endl;
+    cout <<"1. Lihat Data Kendaraan Terparkir saat ini "<<endl;
+    cout <<"2. Registrasi Kendaraan "<<endl;
+    cout <<"3. Perbaharui Data Kendaran Terparkir "<<endl;
+    cout <<"4. Hapus Data Kendaraan "<<endl;
+    
+    cout <<"| -- Menu Tambahan --| "<<endl;
+    cout <<"5. Cari Kendaran berdasarkan jenis Kendaran"<<endl;
+    cout <<"6. Cari Kendaraan berdasarkan Nama Pemilik"<<endl;
+    cout <<"7. Urutkan ber"<<endl;
+
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,29 @@
+/**
+    ** PROGRAM  : SISTEM PARKIR KENDARAAN 
+    ** KELOMPOK : 2
+    -------------------------------------
+
+    ** NAMA ANGGOTA ---------------------
+     * Panggih alman
+     * Masrurah
+     * Rian Imami
+     * Nur Widyanto
+     * Frand Odi
+     * 
+
+*/
+/*  
+    **      ===== Banner.cpp =====
+    * Penyingkatan penulisan daftar perintah
+    * Agar struktur Program tidak Terlalu 
+    * panjang
+*/
+#include "banner.cpp"
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    banner();
+}
